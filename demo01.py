0ccac794c19490ee5bd3a5f4669b8f0d5d8fec47
zhangjialeemail <893831281@qq.com>
2020-10-24 11:05:22 +0800

    第一次提交代码到GIThub

---
+++ demo01.py
@@ -0,0 +1,54 @@
+"""
+print("你好，我叫张家乐") #字符串
+print(2333) #整数
+print(2.333)  # 小数
+print(True)   #布尔值
+print(False)  #布尔值
+print(())    #元祖
+print([])      #数组
+print({})    #字典
+
+
+将进酒杯莫停
+一曲白歌一剑仙
+
+
+print("我爱你",520,52.1)
+print("woaini"+"我爱你")  #字符串的拼接
+print("woaini"*100)       
+print(((1+3)*20-90)/2)
+print((2+0.5)*4/5)        #int 和double 都是数字，可以运算
+print(3<5)               #判断对错
+
+
+
+name = "叶晓倩"
+print(name)
+
+"""
+
+# a =input("请输入：")    #input（）输入的都是字符串类型
+# b =input("请输入：")
+# print("input输入的值:",int(a)+int(b))
+
+# c = type(2.33)      #type() 判断数据类型
+# print(c)
+
+# print(type("叶晓倩"))
+# print(type(520))            ctry+/  注释的快捷键
+# print(type(52.1))
+# print(type(False))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+a = 'asdasdqwdsaascasasacs'
+print(len(a))                 #len()计算字符串长度
+
+"""
+通过代码获取两端内容，并且计算他们的长度
+"""
+a = input("请输入：")
+b = input("请输入：")
+print("两段字符串的长度之和：",len(a)+len(b))
